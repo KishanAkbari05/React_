c7feed9517048aa4fa6251bcf7e0e3bda0bfe2dc
KishanAkbari05 <kishanakbari18@gmail.com>
2024-09-07 17:10:57 +0530

    "Updated padding in InputBox component from p-3 to p-4"

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -18,7 +18,7 @@ function InputBox(
     const amountInputId = useId();
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className={`bg-white p-4 rounded-lg text-sm flex ${className}`}>
 
             <div className="w-1/2">
                 <label 
